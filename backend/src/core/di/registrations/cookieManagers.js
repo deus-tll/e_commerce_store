1023da0f654d01b57cdd646806cf2d9049bdeb34
Artem M. <miller.artem@proton.me>
2026-05-06 13:52:09 +0300

    chore: Add minikube setup for production-like environment

--- backend/src/core/di/registrations/cookieManagers.js
+++ backend/src/core/di/registrations/cookieManagers.js
@@ -10,7 +10,7 @@ import {config} from "../../../config.js";
 const registerCookieManagers = (container) => {
     container.register(CookieManagerTypes.AUTH, () => {
         const dateTimeUtility = container.get(UtilityTypes.DATE);
-        return new AuthCookieManager(dateTimeUtility, config.auth.refresh.ttl, config.app.isProduction);
+        return new AuthCookieManager(dateTimeUtility, config.auth.refresh.ttl, config.app.isProduction, config.app.forceDisableSecureCookies);
     });
 }
 
